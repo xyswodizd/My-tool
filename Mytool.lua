086a91e980eba0ac5de71833233d380a70fcb737
xyswodizd <48475025+xyswodizd@users.noreply.github.com>
2024-11-26 17:37:57 +0800

    Update Mytool.lua

--- Mytool.lua
+++ Mytool.lua
@@ -7,7 +7,34 @@ f:SetScript("OnEvent", function(self, event)
 	TargetFrameTextureFrameName:SetPoint("CENTER", TargetFrameHealthBar, "CENTER", 0, 25)---目标头像名字位置
 	TargetFrameHealthBarTextLeft:SetPoint("left", TargetFrameHealthBar, "left", 1, -1)---在生命值和百分比同时显示时，目标生命值百分比位置
 	TargetFrameHealthBarTextRight:SetPoint("Right", TargetFrameHealthBar, "Right", -5, -1)---在生命值和百分比同时显示时，目标生命值位置
+	TargetFrame.deadText:SetPoint("CENTER", TargetFrameHealthBar, "CENTER", 0, 0)---目标死亡文字显示位置
 end)
+
+PlayerFrame:UnregisterEvent("UNIT_COMBAT")-- 隐藏玩家头像伤害治疗数字
+PetFrame:UnregisterEvent("UNIT_COMBAT")--隐藏宠物头像伤害治疗数字
+hooksecurefunc("GameTooltip_SetDefaultAnchor", function (t,p)t:SetOwner(p, "ANCHOR_CURSOR_RIGHT", 40, -120)end)--鼠标提示信息跟随鼠标锚点
+
+function GameTooltip_UnitColor(unit)-- 鼠标提示名字染色
+	local r, g, b
+	local reaction = UnitReaction(unit, "player")
+	if reaction then
+		r = FACTION_BAR_COLORS[reaction].r
+		g = FACTION_BAR_COLORS[reaction].g
+		b = FACTION_BAR_COLORS[reaction].b
+	else
+		r = 1.0
+		g = 1.0
+		b = 1.0
+	end
+
+	if UnitIsPlayer(unit) then
+		local class = select(2, UnitClass(unit))
+		r = RAID_CLASS_COLORS[class].r
+		g = RAID_CLASS_COLORS[class].g
+		b = RAID_CLASS_COLORS[class].b
+	end
+	return r, g, b
+end
 --下面是移动buff框体部分，但我想要只移动debuff，注释掉后续再研究
 --local Bx = -6-- BUFF的X坐标
 --local By = 30-- BUFF的Y坐标
