11109f57f2a4e317f811a00aa444ba0ead12fbd3
xyswodizd <48475025+xyswodizd@users.noreply.github.com>
2024-11-26 22:35:33 +0800

    Update Mytool.lua

--- Mytool.lua
+++ Mytool.lua
@@ -4,14 +4,16 @@ f:SetScript("OnEvent", function(self, event)
 	PlayerName:SetPoint("CENTER", PlayerFrameHealthBar, "CENTER", 0, 24)---玩家头像名字位置
 	PlayerFrameHealthBarTextLeft:SetPoint("left", PlayerFrameHealthBar, "left", 4, -3)---在生命值和百分比同时显示时，玩家生命值百分比位置
 	PlayerFrameHealthBarTextRight:SetPoint("Right", PlayerFrameHealthBar, "Right", -1, -3)---在生命值和百分比同时显示时，玩家生命值位置
+	PlayerFrameHealthBarText:SetPoint("CENTER", PlayerFrameHealthBar, "CENTER", 0, 0)
 	TargetFrameTextureFrameName:SetPoint("CENTER", TargetFrameHealthBar, "CENTER", 0, 25)---目标头像名字位置
 	TargetFrameHealthBarTextLeft:SetPoint("left", TargetFrameHealthBar, "left", 1, -1)---在生命值和百分比同时显示时，目标生命值百分比位置
 	TargetFrameHealthBarTextRight:SetPoint("Right", TargetFrameHealthBar, "Right", -5, -1)---在生命值和百分比同时显示时，目标生命值位置
 	TargetFrame.deadText:SetPoint("CENTER", TargetFrameHealthBar, "CENTER", 0, 0)---目标死亡文字显示位置
+	PlayerFrame:UnregisterEvent("UNIT_COMBAT")-- 隐藏玩家头像伤害治疗数字
+	PetFrame:UnregisterEvent("UNIT_COMBAT")--隐藏宠物头像伤害治疗数字
 end)
 
-PlayerFrame:UnregisterEvent("UNIT_COMBAT")-- 隐藏玩家头像伤害治疗数字
-PetFrame:UnregisterEvent("UNIT_COMBAT")--隐藏宠物头像伤害治疗数字
+
 hooksecurefunc("GameTooltip_SetDefaultAnchor", function (t,p)t:SetOwner(p, "ANCHOR_CURSOR_RIGHT", 40, -120)end)--鼠标提示信息跟随鼠标锚点
 
 function GameTooltip_UnitColor(unit)-- 鼠标提示名字染色
